fd130515f48ac93c753daad6a8532ed6468fd8a7
cemalfratdag <cemalfratdag@gmail.com>
2024-06-28 01:03:53 +0300

    add DisplayAlbum component

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -22,7 +22,7 @@ const DisplayHome = () => {
         </div>
       </div>
       <div className="mb-4">
-        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        <h1 className="my-5 font-bold text-2xl">{"Today's biggest hits"}</h1>
         <div className="flex overflow-auto">
           {songsData.map((item, index) => (
             <SongItem
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
 
 const Navbar = () => {
+  const navigate = useNavigate()
   return (
     <>
       <div className="w-full flex justify-between items-center font-semibold">
         <div className="flex items-center gap-2">
           <img
-            className="w-8 bg-black p-2 rounded-2xl cursor pointer"
+            onClick={() => navigate(-1)}
+            className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
             src={assets.arrow_left}
             alt=""
           />
           <img
-            className="w-8 bg-black p-2 rounded-2xl cursor pointer"
+            onClick={() => navigate(1)}
+            className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
             src={assets.arrow_right}
             alt=""
           />
@@ -24,7 +28,7 @@ const Navbar = () => {
           <p className="bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer">
             Install App
           </p>
-          <p className="bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center">
+          <p className="bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center cursor-pointer">
             C
           </p>
         </div>
